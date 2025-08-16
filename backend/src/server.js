56c51534e524571d72f2754990b873802d680a92
Priya Gupta <priyagupta052k@gmail.com>
2025-08-16 23:47:20 +0530

    Setup mongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,54 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 const app = express();
 const db = mongoose();
 const PORT = 3000;
 
-// dummy request
-app.get('/', (req,res) =>{
-    res.send("Hello World!");
-    console.log("Received GET request");
-})
+// Middleware
+app.use(express.json());
+app.use(cors());
 
-app.post('/items' , (req,res) =>{
-    res.send("Get a post request");
-})
 
-app.put('/items/:id',(req,res)=>{
-    res.send("Get a put request");
+// MongoDB Connection
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-app.delete('/delete/:id',(req,res) =>{
-    res.send("Get a delete request");
-})
-// app or server
-app.listen(PORT, () =>{
-    console.log("Server has started");
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.log("❌ DB Connection Error:", err));
+
+// Start Server
+app.listen(3000, () => {
+    console.log("Server running on port 5000");
 });
+
+// // dummy request
+// app.get('/', (req,res) =>{
+//     res.send("Hello World!");
+//     console.log("Received GET request");
+// })
+
+// app.post('/items' , (req,res) =>{
+//     res.send("Get a post request");
+// })
+
+// app.put('/items/:id',(req,res)=>{
+//     res.send("Get a put request");
+// })
+// app.delete('/delete/:id',(req,res) =>{
+//     res.send("Get a delete request");
+// })
+// app or server
+// app.listen(PORT, () =>{
+//     console.log("Server has started");
+// });
+
+
+
+
+
+
+
+
+
