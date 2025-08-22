42d22362d702f832fb41a2f2499b8fd3b469b4bf
Priya Gupta <priyagupta052k@gmail.com>
2025-08-22 23:59:06 +0530

    Setup database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,8 @@
 const db = mongoose();
+// // MongoDB Connection
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("DB Connection Error:", err));
