f586f8b57c1c8efa7b2e955d4dca7774edb02614
Priya Gupta <priyagupta052k@gmail.com>
2025-09-11 22:03:50 +0530

    Update MessageModel

---
+++ backend/src/models/MessagesModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { type } = require('server/reply');
+
+
+const MessageModel = mongoose.Schema({
+sender:
+{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user",
+},
+Content : {
+    type:String,
+    trim:true,
+},
+
+chat:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"chat"
+},
+},
+{
+    timestamps:true,
+});
+
+exports.module = mongoose.model("message",MessageModel);
+
+// sender
+// content 
+// chat
--- backend/src/models/UsersModel.js
+++ backend/src/models/UsersModel.js
@@ -23,6 +23,12 @@ email:{
     type:String,
     required:true,
   },
+  picture:{
+    type:String,
+    required:true,
+    default:
+       "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+  },
 },
 {
 timestamps:true
