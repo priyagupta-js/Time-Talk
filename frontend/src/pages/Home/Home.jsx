9ef2f0a86aced2ecaaa8f04f3300fff415ee663a
Priya Gupta <priyagupta052k@gmail.com>
2025-11-25 23:04:10 +0530

    update

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -22,6 +22,8 @@ const Home = () =>{
     <div className="main">
       <div className="navbar">
         {/* logo */}
+        <div className="logo"></div>
+        <div className="">Time-Talk</div>
         {/* project-name */}
       </div>
       <div className="chatUI-wrapper">
