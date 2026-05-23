121567f9278f3da963c408a675037ff07641d652
Priya Gupta <priyagupta052k@gmail.com>
2026-05-24 01:34:11 +0530

    update backend files

--- backend/src/models/ChatModel.js
+++ backend/src/models/ChatModel.js
@@ -1,24 +1,25 @@
+
 const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema(
   {
     isGroupChat: {
       type: Boolean,
-      default: false,
+      default:false,
     },
     users: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required:"true",
+        ref:"User",
+        required:true,
       },
     ],
     lastMessage: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Message",
+      ref:"Message",
     },
   },
-  { timestamps: true }
+  { timestamps:true }
 );
 
 module.exports = mongoose.model("Chat", chatSchema);
